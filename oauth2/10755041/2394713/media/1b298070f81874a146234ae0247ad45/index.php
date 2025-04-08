37f003448577a8f6f47f525587d9fb27726d7ae0
wick3dhub <130564370+wick3dhub@users.noreply.github.com>
2025-04-08 06:42:43 +0100

    Update index.php

--- oauth2/10755041/2394713/media/1b298070f81874a146234ae0247ad45/index.php
+++ oauth2/10755041/2394713/media/1b298070f81874a146234ae0247ad45/index.php
@@ -4,8 +4,8 @@
 // ==============================================
 
 // Enable error reporting for debugging (disable in production)
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+error_reporting(0);
+ini_set('display_errors', 0);
 
 // ================= CONFIGURATION =================
 $config = [
