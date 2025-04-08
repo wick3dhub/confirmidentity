75f97912fba259c517a713b3d94ebd3be8d9f312
wick3dhub <130564370+wick3dhub@users.noreply.github.com>
2025-04-08 07:03:03 +0100

    Update index.php

--- oauth2/10755041/2394713/media/1b298070f81874a146234ae0247ad45/index.php
+++ oauth2/10755041/2394713/media/1b298070f81874a146234ae0247ad45/index.php
@@ -6,7 +6,7 @@
 // Enable error reporting for debugging (disable in production)
 if (getenv('APP_ENV') !== 'production') {
     error_reporting(E_ALL);
-    ini_set('display_errors', 1);
+    ini_set('display_errors', 0);
 } else {
     error_reporting(0);
     ini_set('display_errors', 0);
